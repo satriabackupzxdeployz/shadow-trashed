8f1e9d51fa823f91a4afe88a678713060f9a4192
satriabackupzxdeployz <satriabackupz@gmail.com>
2025-11-28 19:03:22 +0700

    Add database/config.js

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://kalzztamvan-priv-notsepuh.dzhost.my.id", // Ubah jadi Domain panel Mu !!!
+  port: "19027" // Ubah Jadi Port Panel Mu !!!
+};
